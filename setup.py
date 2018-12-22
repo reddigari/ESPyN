2e75befe18ccc08a1c59ac78336a613717484152
Samir Reddigari <samirreddigari@gmail.com>
2018-12-21 21:01:27 -0500

    Add minimal setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='espyn',
+      version='0.1',
+      description="Scrapes and models fantasy leagues using ESPN's private API",
+      url='http://github.com/reddigari/ESPyN',
+      author='Samir Reddigari',
+      author_email='samirreddigari@gmail.com',
+      license='MIT',
+      packages=['espyn'],
+      zip_safe=False)
