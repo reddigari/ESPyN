cbcd598c36812ecf09f3ee4984cf5fc1f4680e17
Samir Reddigari <samirreddigari@gmail.com>
2021-03-24 22:37:45 -0400

    feat: derive filenames within cache objects

--- espyn/caches.py
+++ espyn/caches.py
@@ -3,31 +3,51 @@ import json
 import logging
 
 
-class LocalCache:
+class Cache:
+    def set_league(self, league):
+        self.league = league
+
+    def load(self, scoring_period=None):
+        raise NotImplementedError()
+
+    def save(self, data, scoring_period=None):
+        raise NotImplementedError()
+
+    def _get_filename(self, scoring_period=None):
+        season = self.league.season
+        league_id = self.league.league_id
+        if scoring_period is None:
+            return f"{season}_{league_id}.json"
+        return f"{season}_{league_id}_sp{scoring_period:02d}.json"
+
+
+class LocalCache(Cache):
 
     def __init__(self, cache_dir):
         if not os.path.exists(cache_dir):
             raise ValueError("The given cache directory does not exist.")
         self.cache_dir = cache_dir
 
-    def read_from_cache(self, fname):
+    def load(self, scoring_period=None):
+        fname = self._get_filename(scoring_period)
         fpath = os.path.join(self.cache_dir, fname)
         try:
             with open(fpath, "r") as f:
                 data = json.load(f)
-            logging.info("Read file {} from local cache.".format(fname))
+            logging.info(f"Read file {fname} from local cache.")
             return data
         except:
             return None
 
-    def write_to_cache(self, data, fname):
+    def save(self, data, scoring_period=None):
+        fname = self._get_filename(scoring_period)
         fpath = os.path.join(self.cache_dir, fname)
         with open(fpath, "w") as f:
             json.dump(data, f)
-        logging.info("Wrote file {} to local cache.".format(fname))
+        logging.info(f"Wrote file {fname} to local cache.")
 
 
-class CloudStorageCache:
+class GoogleCloudStorageCache(Cache):
 
     def __init__(self):
         from google.cloud import storage
@@ -44,15 +64,17 @@ class CloudStorageCache:
         except:
             raise RuntimeError("Unable to connect to the specified Cloud Storage bucket.")
 
-    def read_from_cache(self, fname):
+    def load(self, scoring_period=None):
+        fname = self._get_filename(scoring_period)
         blob = self._bucket.get_blob(fname)
         if blob:
             raw = blob.download_as_string()
-            logging.info("Read file {} from Cloud Storage.".format(fname))
+            logging.info(f"Read file {fname} from Cloud Storage.")
             return json.loads(raw)
         return None
 
-    def write_to_cache(self, data, fname):
+    def save(self, data, scoring_period=None):
+        fname = self._get_filename(scoring_period)
         blob = self._bucket.blob(fname)
         blob.upload_from_string(json.dumps(data))
-        logging.info("Wrote file {} to Cloud Storage.".format(fname))
+        logging.info("Wrote file {fname} to Cloud Storage.")
