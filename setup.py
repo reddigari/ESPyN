47c45ac6d922e136fac8bd01aa279dec1a7b5cda
Samir Reddigari <samirreddigari@gmail.com>
2021-04-02 22:16:07 -0400

    build: use pyproject.toml and setup.cfg

--- setup.py
+++ setup.py
@@ -1,11 +1,5 @@
-from setuptools import setup
+import setuptools
 
-setup(name='espyn',
-      version='0.1',
-      description="Scrapes and models fantasy leagues using ESPN's private API",
-      url='http://github.com/reddigari/ESPyN',
-      author='Samir Reddigari',
-      author_email='samirreddigari@gmail.com',
-      license='MIT',
-      packages=['espyn'],
-      zip_safe=False)
+
+setuptools.setup()
+
