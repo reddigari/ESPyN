f4722917e692d01e8b855ffc9d75beb9b66f4847
Samir Reddigari <samirreddigari@gmail.com>
2021-04-03 18:19:02 -0400

    doc: add type hints and docstrings to TeamWeek

--- espyn/team_week.py
+++ espyn/team_week.py
@@ -1,9 +1,21 @@
+from typing import Dict, Any
+
 from .player_week import PlayerWeek
 
 
 class TeamWeek:
+    """Representation of team boxscore for a scoring period"""
 
-    def __init__(self, week_data, scoring_period):
+    def __init__(self, week_data: Dict[str, Any],
+                 scoring_period: int) -> None:
+        """Create team-week instance
+
+        :param week_data: data from API response
+        :type week_data: Dict[str, Any]
+        :param scoring_period: scoring period
+        :type scoring_period: int
+        """
+
         self.scoring_period = scoring_period
         self.points = week_data["pointsByScoringPeriod"][str(scoring_period)]
         self.slots = []
